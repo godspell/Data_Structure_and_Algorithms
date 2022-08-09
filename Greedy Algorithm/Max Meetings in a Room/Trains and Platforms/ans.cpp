e2d467b8edbdaff4511ee971391ef4c00e2f1a8d
Sumeet Haldar <99740668+godspell@users.noreply.github.com>
2022-08-09 15:26:27 +0530

    Update ans.cpp

--- Greedy Algorithm/Max Meetings in a Room/Trains and Platforms/ans.cpp
+++ Greedy Algorithm/Max Meetings in a Room/Trains and Platforms/ans.cpp
@@ -1,1 +1,26 @@
-
+class Solution{
+  public:
+  int findPlatform(int arr[], int dept[], int n)
+    {
+    	// Your code here
+    	sort(arr, arr+n);
+    	sort(dept, dept+n);
+    	
+    	int i=0, j=0, result=0, count =0;
+    	while(i<n && j<n)
+    	{
+    	    if(arr[i]<=dept[j])
+    	    {
+    	        count++;
+    	        i++;
+    	    }
+    	    else
+    	    {
+    	        count--;
+    	        j++;
+    	    }
+    	    result = max(result, count);
+    	}
+    	return result;
+    }
+};
